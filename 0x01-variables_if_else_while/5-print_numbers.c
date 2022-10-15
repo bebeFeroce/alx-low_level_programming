7d81c8dbd30ae3a8044fb3b10c88f04d1541b4cc
Emmanuel <lidaoedjam@gmail.com>
2022-10-14 18:06:42 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print 01234567789
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d", number);
+	}
+
+	return (0);
+}
