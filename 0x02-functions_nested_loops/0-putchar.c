426dbdba8c083c49d5c50e1e7f965a7827b57262
Emmanuel <lidaoedjam@gmail.com>
2022-10-18 16:22:40 -0700

    0. _putchar 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
  * main - Entry point; print _putchar
@@ -7,5 +7,7 @@
  */
 int main(void)
 {
-	printf("_putchar \n")
+	printf("_putchar\n");
+
+	return 0;
 }
