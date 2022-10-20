b9de98b037011271e4500c9b230737259a116635
Emmanuel <lidaoedjam@gmail.com>
2022-10-20 12:53:47 -0700

    1. isdigit 1)

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,12 @@
 # include "main.h"
 
+/**
+ * _isupper - check if c is upper
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if its uppercase, 0 if not
+ */
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
